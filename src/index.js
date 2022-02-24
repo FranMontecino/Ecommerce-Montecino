009a6461f4bca4b687c891a50dacd10703ebf150
Fran Montecino <francomontecino2003@hotmail.com>
2022-02-23 23:01:59 -0300

    Adding Firebase

--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
+import { initializeApp } from "firebase/app";
 
-ReactDOM.render(<BrowserRouter><App/></BrowserRouter>,document.getElementById("root")); 
+const firebaseConfig = {
+  apiKey: "AIzaSyAoTRaMF4WDT97kMB4R_E6HW133_w-J4XQ",
+  authDomain: "gamestate-ecommerce.firebaseapp.com",
+  projectId: "gamestate-ecommerce",
+  storageBucket: "gamestate-ecommerce.appspot.com",
+  messagingSenderId: "652286074567",
+  appId: "1:652286074567:web:23c3ea947104c3b848e8e2"
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+initializeApp(firebaseConfig);
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>,document.getElementById("root")
+); 
+
 reportWebVitals();
